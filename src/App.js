66332f2bb7082ae247d01bba0c04a319eb053097
jonathanprozzi <jonathanprozzi@gmail.com>
2021-06-15 14:33:40 -0400

    swapped out browser router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
+import IpfsRouter from "ipfs-react-router";
 import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <div className='App-container'>
-        <nav>
-          <ul>
-            <li>
-              <Link className='App-link' to='/'>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className='App-link' to='/about'>
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className='App-link' to='/users'>
-                Users
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <div className='App'>
+      <IpfsRouter>
+        <div className='App-container'>
+          <nav>
+            <ul>
+              <li>
+                <Link className='App-link' to='/'>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className='App-link' to='/about'>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className='App-link' to='/users'>
+                  Users
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/users'>
-            <Users />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/users'>
+              <Users />
+            </Route>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </IpfsRouter>
+    </div>
   );
 }
 
